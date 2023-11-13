8319274010265d9bc9be8f5d112974dbbea215bd
Fertilesoil <gasnbloop@gmail.com>
2023-11-13 00:23:11 -0300

    Criando o Projeto e Introdução à Props

---
+++ React/estudo-ada/src/components/first_comp/First_Comp.tsx
@@ -0,0 +1,8 @@
+﻿
+function First_Comp(props) {
+  return (
+    <div className="flex justify-center items-center text-2xl text-center">Ora ora se não são meus primeiros componentes { props.conteudo}</div>
+  )
+}
+
+export default First_Comp
